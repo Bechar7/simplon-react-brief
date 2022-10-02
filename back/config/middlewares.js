9af8973e11b1a2eebb7c5d3547f991d19754a202
Sylvain <sylvain.conny@protonmail.com>
2022-10-02 16:45:58 +0200

    supabase buckets

--- back/config/middlewares.js
+++ back/config/middlewares.js
@@ -1,6 +1,16 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "default-src": ["'self'"],
+          "img-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL")],
+        },
+      },
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
